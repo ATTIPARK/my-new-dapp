63fa22d60338cc3325895010e0ce751ed3fe164a
ATTIPARK <qkrqjawls0922@gmail.com>
2023-03-30 11:13:27 +0900

    Favorite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <h2>My name is bumjin!</h2>
+      <h3>My Favorite drink is Zero Coke</h3>
+    </div>
+  );
+}
+
+export default App;
