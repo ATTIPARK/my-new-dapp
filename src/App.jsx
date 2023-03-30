ea6212a32833021e5cde8480c51cf28d3afdf152
ATTIPARK <qkrqjawls0922@gmail.com>
2023-03-30 11:24:56 +0900

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <h1 className="hello">Hello, React!</h1>
       <h2 className="name">My name is bumjin!</h2>
-      <h3 className="favorite">My Favorite drink is Zero Coke</h3>
+      <h3 className="favorite">My Favorite drink is Zero Coke 🥤</h3>
     </div>
   );
 }
